eb5c95bc7448611d3110701642f65378e0a68bf8
Danrley Lima <danrleylac@gmail.com>
2024-03-24 16:27:29 -0300

    feat: add initial spring boot project files

---
+++ src/main/java/com/web2/booking/BookingApplication.java
@@ -0,0 +1,21 @@
+package com.web2.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BookingApplication {
+
+	@RequestMapping("/")
+	public String test() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web2/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web2.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
