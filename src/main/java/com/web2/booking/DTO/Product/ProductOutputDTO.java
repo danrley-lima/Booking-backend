a0e5809bcbaefb6eb39123f90ae799d02ae797ef
Danrley Lima <danrleylac@gmail.com>
2024-04-27 08:32:57 -0300

    feat: add ProductOutputDTO

---
+++ src/main/java/com/web2/booking/DTO/Product/ProductOutputDTO.java
@@ -0,0 +1,9 @@
+package com.web2.booking.DTO.Product;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ProductOutputDTO(UUID id, String title, String city, String state, String image, double price,
+        double customerScore, int numberOfReviews, int discount, double totalPrice, LocalDateTime createdAt) {
+
+}
