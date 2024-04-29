9f95f441a0e8d7363452fd1f8f01b6ba0bf9819d
Danrley Lima <danrleylac@gmail.com>
2024-04-29 17:46:30 -0300

    test: add unit tests for ProductRepository

---
+++ src/test/java/com/web2/booking/repositories/ProductRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.web2.booking.repositories;
+
+import java.time.LocalDateTime;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.web2.booking.models.ProductModel;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class ProductRepositoryTest {
+
+  private ProductModel product;
+
+  @BeforeEach
+  public void setUp() {
+    product = new ProductModel();
+    product.setTitle("Test Product");
+    product.setPrice(100.0);
+    product.setCity("Test City");
+    product.setDiscount(20);
+    product.setImage("blablabla");
+    product.setNumberOfReviews(12);
+    product.setState("RN");
+    product.setTotalPrice(3200);
+    product.setCustomerScore(8.5);
+    product.setCreatedAt(LocalDateTime.of(2024, 01, 20, 0, 0));
+  }
+
+  @Autowired
+  private ProductRepository productRepository;
+
+  @Test
+  @DisplayName("Save Product Test")
+  void testSaveProduct() {
+    ProductModel savedProduct = productRepository.save(product);
+    Assertions.assertThat(savedProduct).isNotNull();
+    Assertions.assertThat(savedProduct.getId()).isNotNull();
+  }
+}
