20bd94d0542eab0106278c74a2c2e5b7b1885e01
Danrley Lima <danrleylac@gmail.com>
2024-05-18 13:15:31 -0300

    feat: add @ManyToOne relationship for CustomerModel

--- src/main/java/com/web2/booking/models/ProductModel.java
+++ src/main/java/com/web2/booking/models/ProductModel.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -56,5 +58,9 @@ public class ProductModel {
 
   @Column(name = "created_at")
   private LocalDateTime createdAt;
+
+  @ManyToOne
+  @JoinColumn(name = "customer_id")
+  private CustomerModel customer;
 
 }
