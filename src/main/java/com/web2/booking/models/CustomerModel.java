29f8b5ac2a69c69a9f9766641ccced0b938576c2
Vanessa <vanessa.maria@imd.ufrn.br>
2024-05-18 11:55:02 -0300

    fix: adjustment in CustomerModel import

--- src/main/java/com/web2/booking/models/CustomerModel.java
+++ src/main/java/com/web2/booking/models/CustomerModel.java
@@ -5,13 +5,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
