6afdbb0f953cd6128cd608d2d839c3bb92e1f234
Danrley Lima <danrleylac@gmail.com>
2024-05-18 14:02:11 -0300

    refactor: optimize imports in CustomerModel

--- src/main/java/com/web2/booking/models/CustomerModel.java
+++ src/main/java/com/web2/booking/models/CustomerModel.java
@@ -4,8 +4,15 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
-
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
