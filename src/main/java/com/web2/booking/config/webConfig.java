b1f8679e815027f085a37bc86ecb7d668c001ce8
Danrley Lima <danrleylac@gmail.com>
2024-06-03 20:52:58 -0300

    feat: Add CORS configuration to enable cross-origin requests

---
+++ src/main/java/com/web2/booking/config/webConfig.java
@@ -0,0 +1,17 @@
+package com.web2.booking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class webConfig implements WebMvcConfigurer {
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedHeaders("*")
+        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+  }
+}
